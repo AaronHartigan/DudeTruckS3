b2423ec4c9942071e417ec3411dfa5711deccf97
Aaron Hartigan <hartiganaaron@gmail.com>
2017-09-13 16:11:27 -0700

    updated favicon:

--- deploy.py
+++ deploy.py
@@ -5,26 +5,31 @@ import os
 import sys
 
 if __name__ == "__main__":
-	s3 = boto3.resource('s3', 'us-west-1')
+	s3 = boto3.resource(
+		's3',
+		'us-west-1',
+		aws_access_key_id=config.aws['accessKeyId'],
+		aws_secret_access_key=config.aws['secretAccessKey'],
+	)
 	bucket = s3.Bucket('dudetruck')
 	
 	filesUploaded = 0
 	print('Loading files to AWS...')
-	for file in os.listdir(''):
+	for file in os.listdir('./'):
 		if file.endswith('.json'):
-			fileData = open(filePath + file, 'rb')
+			fileData = open(file, 'rb')
 			bucket.put_object(Key=file, ACL='public-read', Body=fileData, ContentType='application/json')
 			filesUploaded += 1
 		elif file.endswith('.html'):
-			fileData = open(filePath + file, 'rb')
+			fileData = open(file, 'rb')
 			bucket.put_object(Key=file, ACL='public-read', Body=fileData, ContentType='text/html')
 			filesUploaded += 1
 		elif file.endswith('.css'):
-			fileData = open(filePath + file, 'rb')
+			fileData = open(file, 'rb')
 			bucket.put_object(Key=file, ACL='public-read', Body=fileData, ContentType='text/css')
 			filesUploaded += 1
 		elif file.endswith('.js'):
-			fileData = open(filePath + file, 'rb')
+			fileData = open(file, 'rb')
 			bucket.put_object(Key=file, ACL='public-read', Body=fileData, ContentType='application/javascript')
 			filesUploaded += 1
 	print('Successfully loaded', filesUploaded, 'files.\n')
